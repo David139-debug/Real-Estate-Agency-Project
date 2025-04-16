d849e991e544a31763b392bf63e8e08ee56c501b
David139-debug <davidmalesevic55@gmail.com>
2025-04-16 19:32:16 +0200

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -18,6 +18,8 @@ import getUsers from "./routes/getAllUsers";
 dotenv.config();
 connectDB();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(cookieParser());
 
@@ -41,7 +43,7 @@ app.use("/getUsers", getUsers);
 
 mongoose.connection.once("open", () => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
         console.log("Server running on port", process.env.PORT);
     });
 });
