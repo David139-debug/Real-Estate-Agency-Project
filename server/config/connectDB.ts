3a945a3a9da730a5f176234b655b4f0cc9fcfdb6
David139-debug <davidmalesevic55@gmail.com>
2025-04-16 19:36:32 +0200

    Update connectDB.ts

--- server/config/connectDB.ts
+++ server/config/connectDB.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("radi")
     } catch (err) {
         console.log("Error occurred.");
     }
