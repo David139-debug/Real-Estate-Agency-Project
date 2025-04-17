e1901e2bba10f6c2c0b16adbff830c6bfd72265d
David139-debug <davidmalesevic55@gmail.com>
2025-04-17 21:55:49 +0200

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -41,9 +41,10 @@ app.use("/deleteProperty", deleteProperty);
 app.use("/editProfile", editProfile);
 app.use("/getUsers", getUsers);
 
+app.listen(PORT, () => {
+    console.log("Server running on port", process.env.PORT);
+});
+
 mongoose.connection.once("open", () => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-        console.log("Server running on port", process.env.PORT);
-    });
 });
